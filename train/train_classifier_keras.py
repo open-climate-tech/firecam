1c95fcb3748c929b0c385b929250beb764fafcb8
Henry Pinkard <henry.pinkard@gmail.com>
2020-03-26 18:30:20 -0700

    fancier keras models

--- train/train_classifier_keras.py
+++ train/train_classifier_keras.py
@@ -4,7 +4,7 @@ from tensorflow import keras
 import collect_args
 import goog_helper
 
-def _parse_function(example_proto):
+def _parse_function(example_proto, sequence_length=None):
     """
     Function for converting TFRecordDataset to uncompressed image pixels + labels
     :return:
@@ -21,16 +21,72 @@ def _parse_function(example_proto):
     example = tf.io.parse_single_example(example_proto, feature_description)
     image = tf.image.decode_image(example['image/encoded'], channels=3)
 
-    #Resizing images in training set because they are apprently rectangular much fo the time
+    #Resizing images in training set because they are apprently rectangular much of the time
     if example['image/height'] != 299 or example['image/width'] != 299:
         image = tf.image.resize(tf.reshape(image, [example['image/height'], example['image/width'], 3]), [299, 299])
         image = tf.cast(image, tf.uint8)
 
-    image = tf.reshape(image, [299, 299, 3]) #weird workaround because decode image doesnt get shape
     label = tf.one_hot(example['image/class/label'], depth=2)
-
+    image = tf.reshape(image, [299, 299, 3]) #weird workaround because decode image doesnt get shape
     image = (tf.cast(image, tf.float32) - 128) / 128.0
-    return [image, label]
+
+    if sequence_length is None:
+        #single image mode
+        return [image, label]
+    else:
+        #sequence of images mode
+        #TODO: THIS IS A PLACEHOLDER TO BE REPLACED WHEN A TRAINING SET OF IMAGE SEQUENCES IS READY
+        #Right now, we just duplicate a single image several time, so the input tensor has the same
+        #dimensionality as it would when real sequences are available
+        image_sequence = tf.stack(sequence_length * [image], axis=0)
+        return [image_sequence, label]
+
+def build_model(type, sequence_length=None):
+    """
+    Construct a model. Can either take in a batch of images (i.e. N,H,W,C tensor) or a batch of time
+    series (i.e. N,T,H,W,C) depending on the specific model)
+    :param type: which model architecture
+    :param sequence_length: how many images in sequence (needed for convolutional sequence models)
+    :return:
+    """
+    if type == 'inception':
+        #Vanilla inception model in which image is fed in and a prediction comes out
+        return keras.applications.inception_v3.InceptionV3(weights=None, include_top=True, input_tensor=None, classes=2)
+
+        keras.Model(inputs=[input_sequences], outputs=[output])
+    elif type == 'inception_rnn':
+        #Inception model applied to each image in a sequence, resulting sequence of vectors is passed into an LSTM
+        inception = keras.applications.inception_v3.InceptionV3(weights=None, include_top=False, input_tensor=None,
+                                                                pooling='max')
+        input_sequences = keras.layers.Input(shape=inception.input_shape)
+        #apply the same inception network to every input in the sequence
+        time_series = keras.layers.TimeDistributed(inception)(input_sequences)
+
+        rnn_state = keras.layers.LSTM(units=1024)(time_series)
+        #2-class
+        output = keras.layers.Dense(2, activation='softmax')(rnn_state)
+        return keras.Model(inputs=[input_sequences], outputs=[output])
+    elif type == 'inception_cnn':
+        #Inception model applied to each image in a sequence, then results are concatenated, and passed through two more
+        # convolutional layers prior to prediction
+        inception = keras.applications.inception_v3.InceptionV3(weights=None, include_top=False, input_tensor=None,
+                                                                pooling=None)
+        input_sequences = keras.layers.Input(shape=[None, *inception.input_shape[1:]])
+        # apply the same inception network to every input in the sequence
+        time_series = keras.layers.TimeDistributed(inception)(input_sequences)
+
+        activation_list = [time_series[:, index] for index in range(sequence_length)]
+        #concatenate the activations from each image into a single tensor
+        all_activations = keras.layers.Concatenate(axis=3)(activation_list)
+        #pass through two conv layers
+        convs = keras.layers.Conv2D(2048, 3)(all_activations)
+        convs = keras.layers.Conv2D(2048, 3)(convs)
+        pooled = keras.layers.GlobalAveragePooling2D()(convs)
+        output = keras.layers.Dense(2, activation='softmax')(pooled)
+        model = keras.Model(inputs=[input_sequences], outputs=[output])
+        return model
+    else:
+        raise Exception('Unknown model architecture')
 
 
 def main():
@@ -44,33 +100,41 @@ def main():
 
     args = collect_args.collectArgs(reqArgs, optionalArgs=optArgs, parentParsers=[goog_helper.getParentParser()])
 
+    #how many images are used in the sequence. RNN models allow this number to vary, so this could be replaced
+    #by a boolean for sequence vs not sequence.
+    # sequence_length = None
+    # architecture = 'inception'
+
+    sequence_length = 3
+    # architecture = 'inception_rnn'
+    architecture = 'inception_cnn'
+
     batch_size = 32
     max_epochs = 1000
     steps_per_epoch=250
     overshoot_epochs=15 #number of epochs over which validation loss hasnt decreased to stop training at
-    val_steps = 100 #only needed for now because of a bug in tf2.0, which should be fixed in next version
-    #TODO: either set this to # of validation examples /batch size (i.e. figure out num validation examples)
-    #or upgrade to TF2.1 when its ready and automatically go thorugh the whole set
 
     train_filenames = glob.glob(args.inputDir + 'firecam_train_*.tfrecord')
     val_filenames = glob.glob(args.inputDir + 'firecam_validation_*.tfrecord')
 
+
     raw_dataset_train = tf.data.TFRecordDataset(train_filenames)
     raw_dataset_val = tf.data.TFRecordDataset(val_filenames)
 
-    dataset_train = raw_dataset_train.map(_parse_function).repeat(max_epochs * steps_per_epoch).shuffle(batch_size * 5).batch(batch_size)
-    dataset_val = raw_dataset_val.map(_parse_function).repeat().batch(batch_size)
+    parse_fn = lambda x: _parse_function(x, sequence_length)
+    dataset_train = raw_dataset_train.map(parse_fn).repeat(max_epochs * steps_per_epoch).shuffle(batch_size * 5).batch(batch_size)
+    dataset_val = raw_dataset_val.map(parse_fn).batch(batch_size)
 
-    inception = keras.applications.inception_v3.InceptionV3(weights=None, include_top=True, input_tensor=None,
-                                                            classes=2)
+    model = build_model(architecture, sequence_length if architecture == 'inception_cnn' else None)
+
     optimizer = tf.keras.optimizers.Adam(learning_rate=0.001, beta_1=0.9, beta_2=0.999, amsgrad=False)
-    inception.compile(optimizer=optimizer, loss=tf.keras.losses.BinaryCrossentropy(), metrics=['accuracy'])
+    model.compile(optimizer=optimizer, loss=tf.keras.losses.BinaryCrossentropy(), metrics=['accuracy'])
 
     callbacks = [keras.callbacks.EarlyStopping(monitor='val_loss', patience=overshoot_epochs),
-                 keras.callbacks.ModelCheckpoint(filepath=args.outputDir + 'best_model',
+                 keras.callbacks.ModelCheckpoint(filepath=args.outputDir + 'best_model_' + architecture,
                                                  monitor='val_loss', save_best_only=True)]
 
-    inception.fit(dataset_train, validation_data=dataset_val, epochs=max_epochs, validation_steps=val_steps, steps_per_epoch=steps_per_epoch, callbacks=callbacks)
+    model.fit(dataset_train, validation_data=dataset_val, epochs=max_epochs, steps_per_epoch=steps_per_epoch, callbacks=callbacks)
 
 
 if __name__ == "__main__":
