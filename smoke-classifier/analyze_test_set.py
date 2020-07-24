74cce1e4b744a8db699efc239aa437b2d9343b13
Kinshuk Govil <kinshuk@gmail.com>
2020-07-24 15:50:53 -0700

    Analyze test set with offset squares

--- smoke-classifier/analyze_test_set.py
+++ smoke-classifier/analyze_test_set.py
@@ -53,73 +53,56 @@ def segmentImage(imgPath):
     
 def classifyImages(detectionPolicy, imageList, className, outFile):
     count = 0
-    image_name = []
-    crop_name = []
-    score_name = []
-    class_name = []
     positives = []
     negatives = []
-    try:
-        for image in imageList:
-            t0 = time.time()
-            isPositive = False
-            ppath = pathlib.PurePath(image)
-            nameParsed = img_archive.parseFilename(image)
+    mixed = []
+    for image in imageList:
+        t0 = time.time()
+        ppath = pathlib.PurePath(image)
+        nameParsed = img_archive.parseFilename(image)
 
-            image_spec = [{}]
-            image_spec[-1]['path'] = image
-            image_spec[-1]['timestamp'] = nameParsed['unixTime']
-            image_spec[-1]['cameraID'] = nameParsed['cameraID']
+        image_spec = [{}]
+        image_spec[-1]['path'] = image
+        image_spec[-1]['timestamp'] = nameParsed['unixTime']
+        image_spec[-1]['cameraID'] = nameParsed['cameraID']
 
-            try:
-                detectionResult = detectionPolicy.detect(image_spec)
-                if detectionResult['fireSegment']:
-                    isPositive = True
-                if detectionResult['segments']:
-                    segments = detectionResult['segments']
-                    for i in range(len(segments)):
-                        image_name += [ppath.name]
-                        crop_name += [segments[i]['coordStr']]
-                        # for testing
-                        # segments[i]['score'] = random.random()*.55
-                        score_name += [segments[i]['score']]
-                        class_name += [className]
-                        if segments[i]['score'] > .5:
-                            isPositive = True
+        detectionResult = detectionPolicy.detect(image_spec, silent=True)
+        image_spec[-1]['startY'] = 140
+        image_spec[-1]['endY'] = -140
+        detectionResultOffset = detectionPolicy.detect(image_spec, silent=True)
+        scores = [detectionResult['segments'][0]['score'], detectionResultOffset['segments'][0]['score']]
+        if detectionResult['fireSegment'] and detectionResultOffset['fireSegment']:
+            status = 'smoke'
+            positives.append(ppath.name)
+        elif (not detectionResult['fireSegment']) and (not detectionResultOffset['fireSegment']):
+            status = 'other'
+            negatives.append(ppath.name)
+        else:
+            status = 'mixed'
+            mixed.append(ppath.name)
 
-            except Exception as e:
-                logging.error('FAILURE processing %s. Count: %d, Error: %s', image, count, str(e))
-                test_data = [image_name, crop_name, score_name, class_name]
-                np.savetxt(outFile + '-ERROR-' + image + '.txt', np.transpose(test_data), fmt = "%s")
-                sys.exit()
-
-            t2 = time.time()
-
-            count += 1
-            if isPositive:
-                positives.append(ppath.name)
-            else:
-                negatives.append(ppath.name)
-            sys.stdout.write('\r>> Caclulated %d/%d of class %s' % (
-                count, len(imageList), className))
-            # logging.warning('Timing %f: %f, %f' % (t2-t0, t1-t0, t2-t1))
-            sys.stdout.flush()
-    except Exception as e:
-        logging.error('Failure after %d images of class %s. Error: %s', count, className, str(e))
-        try:
-            test_data = [image_name, crop_name, score_name, class_name]
-            np.savetxt(outFile + '-ERROR.txt', np.transpose(test_data), fmt = "%s")
-        except Exception as e:
-            logging.error('Total Failure, Moving On. Error: %s', str(e))
+        t2 = time.time()
+        count += 1
+        sys.stdout.write('\r>> Caclulated %d/%d of class %s' % (
+            count, len(imageList), className))
+        # logging.warning('Timing %f: %f, %f' % (t2-t0, t1-t0, t2-t1))
+        sys.stdout.flush()
+        outFile.write('%s file %s classified as %s: %s\n' % (
+            className, ppath.name, status, str(scores)))
     sys.stdout.write('\n')
     sys.stdout.flush()
-    return (image_name, crop_name, score_name, class_name, positives, negatives)
+    return (positives, negatives, mixed)
 
 
 def safeDiv(dividend, divisor):
     if divisor == 0:
         return 0
     return dividend / divisor
+
+
+def doubleOut(outFile, msg):
+    logging.warning(msg)
+    outFile.write(msg + '\n')
 
 
 def main():
@@ -139,18 +122,6 @@ def main():
     DetectionPolicyClass = policies.get_policies()[settings.detectionPolicy]
     detectionPolicy = DetectionPolicyClass(args, None, 0, stateless=True, modelLocation=model_file)
 
-    test_data = []
-
-    image_name = []
-    crop_name = []
-    score_name = []
-    class_name = []
-
-    image_name += ["Image"]
-    crop_name += ["Crop"]
-    score_name += ["Score"]
-    class_name += ["Class"]
-
     smokeDir = os.path.join(args.directory, 'test_set_smoke')
     smoke_image_list = listJpegs(smokeDir)
     logging.warning('Found %d images of smoke', len(smoke_image_list))
@@ -158,49 +129,40 @@ def main():
     other_image_list = listJpegs(nonSmokeDir)
     logging.warning('Found %d images of nonSmoke', len(other_image_list))
 
-    smokeFile = os.path.join(args.directory, 'test_smoke.txt')
-    np.savetxt(smokeFile, smoke_image_list, fmt = "%s")
-    nonSmokeFile = os.path.join(args.directory, 'test_other.txt')
-    np.savetxt(nonSmokeFile, other_image_list, fmt = "%s")
     outFile = open(args.outputFile, 'w')
+    doubleOut(outFile, 'Checking model %s' % model_file)
 
-    (i,cr,s,cl, positives, negatives) = classifyImages(detectionPolicy, smoke_image_list, 'smoke', args.outputFile)
-    image_name += i
-    crop_name += cr
-    score_name += s
-    class_name += cl
+    (positives, negatives, mixed) = classifyImages(detectionPolicy, smoke_image_list, 'smoke', outFile)
     logging.warning('Done with smoke images')
+    doubleOut(outFile, 'Smoke counts (pos,neg,mixed): %d, %d, %d' % (len(positives), len(negatives), len(mixed)))
     truePositive = len(positives)
-    falseNegative = len(smoke_image_list) - len(positives)
+    falseNegative = len(negatives) + len(mixed)
     logging.warning('True Positive: %d', truePositive)
     logging.warning('False Negative: %d', falseNegative)
     outFile.write('True Positives: ' + ', '.join(positives) + '\n')
     outFile.write('False Negative: ' + ', '.join(negatives) + '\n')
+    outFile.write('Mixed smoke: ' + ', '.join(mixed) + '\n')
 
-    (i,cr,s,cl, positives, negatives) = classifyImages(detectionPolicy, other_image_list, 'other', args.outputFile)
-    image_name += i
-    crop_name += cr
-    score_name += s
-    class_name += cl
+    (positives, negatives, mixed) = classifyImages(detectionPolicy, other_image_list, 'other', outFile)
     logging.warning('Done with nonSmoke images')
-    falsePositive = len(positives)
-    trueNegative = len(other_image_list) - len(positives)
+    doubleOut(outFile, 'nonSmoke counts (pos,neg,mixed): %d, %d, %d' % (len(positives), len(negatives), len(mixed)))
+    falsePositive = len(positives) + len(mixed)
+    trueNegative = len(negatives)
     logging.warning('False Positive: %d', falsePositive)
     logging.warning('True Negative: %d', trueNegative)
     outFile.write('False Positives: ' + ', '.join(positives) + '\n')
     outFile.write('True Negative: ' + ', '.join(negatives) + '\n')
+    outFile.write('Mixed nonSmoke: ' + ', '.join(mixed) + '\n')
 
     accuracy = safeDiv(truePositive + trueNegative, truePositive + trueNegative + falsePositive + falseNegative)
-    logging.warning('Accuracy: %f', accuracy)
+    doubleOut(outFile, 'Accuracy: %f' % accuracy)
     precision = safeDiv(truePositive, truePositive + falsePositive)
-    logging.warning('Precision: %f', precision)
+    doubleOut(outFile, 'Precision: %f' % precision)
     recall = safeDiv(truePositive, truePositive + falseNegative)
-    logging.warning('Recall: %f', recall)
+    doubleOut(outFile, 'Recall: %f' % recall)
     f1 = safeDiv(2 * precision*recall, precision + recall)
-    logging.warning('F1: %f', f1)
+    doubleOut(outFile, 'F1: %f' % f1)
 
-    test_data = [image_name, crop_name, score_name, class_name]
-    np.savetxt(outFile, np.transpose(test_data), fmt = "%s")
     outFile.close()
     print("DONE")
 
