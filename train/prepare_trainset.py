ae33c0164b6d244a3c57e95f703a64d751f15343
Kinshuk Govil <kinshuk@gmail.com>
2020-10-05 21:57:03 -0700

    Update training code for TF2

--- train/prepare_trainset.py
+++ train/prepare_trainset.py
@@ -42,7 +42,6 @@ from __future__ import print_function
 
 import os, sys
 from firecam.lib import collect_args
-from firecam.lib import goog_helper
 
 import math
 import random
@@ -98,30 +97,10 @@ def write_label_file(labels_to_class_names, dataset_dir,
         filename: The filename where the class names are written.
     """
     labels_filename = os.path.join(dataset_dir, filename)
-    with tf.gfile.Open(labels_filename, 'w') as f:
+    with tf.io.gfile.GFile(labels_filename, 'w') as f:
         for label in labels_to_class_names:
             class_name = labels_to_class_names[label]
             f.write('%d:%s\n' % (label, class_name))
-
-
-class ImageReader(object):
-    """Helper class that provides TensorFlow image coding utilities."""
-
-    def __init__(self):
-        # Initializes function that decodes RGB JPEG data.
-        self._decode_jpeg_data = tf.placeholder(dtype=tf.string)
-        self._decode_jpeg = tf.image.decode_jpeg(self._decode_jpeg_data, channels=3)
-
-    def read_image_dims(self, sess, image_data):
-        image = self.decode_jpeg(sess, image_data)
-        return image.shape[0], image.shape[1]
-
-    def decode_jpeg(self, sess, image_data):
-        image = sess.run(self._decode_jpeg,
-                        feed_dict={self._decode_jpeg_data: image_data})
-        assert len(image.shape) == 3
-        assert image.shape[2] == 3
-        return image
 
 
 def _get_filenames_and_classes(dataset_dir):
@@ -173,33 +152,26 @@ def _convert_dataset(split_name, filenames, class_names_to_ids, dataset_dir):
     numShards = int(math.ceil(len(filenames) / 9000)) # 9000 images results in ~90MB shards
     num_per_shard = int(math.ceil(len(filenames) / float(numShards)))
 
-    with tf.Graph().as_default():
-        image_reader = ImageReader()
+    for shard_id in range(numShards):
+        output_filename = _get_dataset_filename(
+            dataset_dir, split_name, shard_id, numShards)
 
-        with tf.Session('') as sess:
+        with tf.io.TFRecordWriter(output_filename) as tfrecord_writer:
+            start_ndx = shard_id * num_per_shard
+            end_ndx = min((shard_id+1) * num_per_shard, len(filenames))
+            for i in range(start_ndx, end_ndx):
+                sys.stdout.write('\r>> Converting image %d/%d shard %d' % (
+                    i+1, len(filenames), shard_id))
+                sys.stdout.flush()
 
-            for shard_id in range(numShards):
-                output_filename = _get_dataset_filename(
-                    dataset_dir, split_name, shard_id, numShards)
+                image_data = tf.io.gfile.GFile(filenames[i], 'rb').read()
+                image_shape = tf.image.decode_jpeg(image_data).shape
+                class_name = os.path.basename(os.path.dirname(filenames[i]))
+                class_id = class_names_to_ids[class_name]
 
-                with tf.python_io.TFRecordWriter(output_filename) as tfrecord_writer:
-                    start_ndx = shard_id * num_per_shard
-                    end_ndx = min((shard_id+1) * num_per_shard, len(filenames))
-                    for i in range(start_ndx, end_ndx):
-                        sys.stdout.write('\r>> Converting image %d/%d shard %d' % (
-                            i+1, len(filenames), shard_id))
-                        sys.stdout.flush()
-
-                        # Read the filename:
-                        image_data = tf.gfile.FastGFile(filenames[i], 'rb').read()
-                        height, width = image_reader.read_image_dims(sess, image_data)
-
-                        class_name = os.path.basename(os.path.dirname(filenames[i]))
-                        class_id = class_names_to_ids[class_name]
-
-                        example = image_to_tfexample(
-                            image_data, b'jpg', height, width, class_id)
-                        tfrecord_writer.write(example.SerializeToString())
+                example = image_to_tfexample(
+                    image_data, b'jpg', image_shape[0], image_shape[1], class_id)
+                tfrecord_writer.write(example.SerializeToString())
 
     sys.stdout.write('\n')
     sys.stdout.flush()
@@ -244,7 +216,7 @@ def main():
         ["t", "trainPercentage", "percentage of data to use for training vs. validation (default 90)"]
     ]
     
-    args = collect_args.collectArgs(reqArgs, optionalArgs=optArgs, parentParsers=[goog_helper.getParentParser()])
+    args = collect_args.collectArgs(reqArgs, optionalArgs=optArgs)
     trainPercentage = int(args.trainPercentage) if args.trainPercentage else 90
 
     writeTFRecords(args.inputDir, args.outputDir, trainPercentage)
